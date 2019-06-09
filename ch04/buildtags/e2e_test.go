4ad5aa4dd51fc3114654ff1972cb2e12a5837e8e
Achilleas Anagnostopoulos <achilleas.imap.mbox@gmail.com>
2019-06-09 22:55:14 +0100

    ch04: add example for using build tags to exclude groups of tests

---
+++ ch04/buildtags/e2e_test.go
@@ -0,0 +1,9 @@
+// +build e2e_tests all_tests
+
+package all_test
+
+import "testing"
+
+func TestE2R(t *testing.T) {
+	t.Logf("Ran end to end test")
+}
---
+++ ch04/buildtags/unit_test.go
@@ -0,0 +1,9 @@
+// +build unit_tests all_tests
+
+package all_test
+
+import "testing"
+
+func TestUnit(t *testing.T) {
+	t.Logf("Ran unit test")
+}
